54cce9422e92034be9858828a9f377dbb731a367
Shaun Hammill <shaun.hammill@mpiresearch.com>
2016-08-11 14:23:41 -0400

    Rescope test item

--- spec/RobotComponent.spec.ts
+++ spec/RobotComponent.spec.ts
@@ -2,8 +2,8 @@ import {RobotComponent} from "../src/Objects/RobotComponent";
 import {ComponentTypes}  from "../src/Enum/ComponentTypes";
 
 describe("RobotComponent", () => {
-	let testedRC = new RobotComponent(ComponentTypes.Head, 5, 7);
 	it("should build a head component to our specification", () => {
+		let testedRC = new RobotComponent(ComponentTypes.Head, 5, 7);
 		expect(testedRC.getType).toEqual(ComponentTypes.Head);
 		expect(testedRC.getSpeed()).toEqual(5);
 		expect(testedRC.getStrength()).toEqual(7);
