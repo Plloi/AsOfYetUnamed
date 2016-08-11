4bb0861f57fb4609bf00f976779ed284b879844e
Shaun Hammill <shaun.hammill@mpiresearch.com>
2016-08-11 11:08:21 -0400

    Added accessors for Type/Strength/Speed

--- src/Objects/RobotComponent.ts
+++ src/Objects/RobotComponent.ts
@@ -3,6 +3,9 @@ import {ComponentTypes}  from "../Enum/ComponentTypes";
 export class RobotComponent {
 
 	constructor(private type: number, private strength: number, private speed: number) {
-		// if
 	}
+
+	getType = () =>  this.type;
+	getSpeed = () => this.speed;
+	getStrength = () => this.strength;
 }
