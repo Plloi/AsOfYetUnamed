c40edb3119e9e48636619ada653bb678dd0cd158
Shaun Hammill <plloi.pllex@gmail.com>
2016-08-15 22:42:49 -0400

    setup test runner/correct tests

--- spec/Robot.spec.ts
+++ spec/Robot.spec.ts
@@ -1,19 +1,19 @@
 import {Robot} from "../src/Objects/Robot";
+import {RobotComponent} from "../src/Objects/RobotComponent";
 import {ComponentTypes}  from "../src/Enum/ComponentTypes";
 
 describe("Robot", () => {
+	let parts = [
+		new RobotComponent(ComponentTypes.Head, 5, 7),
+		new RobotComponent(ComponentTypes.LeftArm, 5, 7),
+		new RobotComponent(ComponentTypes.RightArm, 5, 7),
+		new RobotComponent(ComponentTypes.LeftLeg, 5, 7),
+		new RobotComponent(ComponentTypes.RightLeg, 5, 7)
+	];
+	let testRobot: Robot;
+
 	it("should build robot from and arrary of components", () => {
-		let parts = [
-			new RobotComponent(ComponentTypes.Head, 5, 7),
-			new RobotComponent(ComponentTypes.LeftArm, 5, 7),
-			new RobotComponent(ComponentTypes.RightArm, 5, 7),
-			new RobotComponent(ComponentTypes.LeftLeg, 5, 7),
-			new RobotComponent(ComponentTypes.RightLeg, 5, 7)
-		];
-		let testRobot = new Robot(parts);
-		expect(testedRC.getType).toEqual(ComponentTypes.Head);
-		expect(testedRC.getSpeed()).toEqual(5);
-		expect(testedRC.getStrength()).toEqual(7);
+		testRobot = new Robot(parts);
 	});
 
 	it("shoould have a speed of 35", () => {
--- spec/RobotComponent.spec.ts
+++ spec/RobotComponent.spec.ts
@@ -4,8 +4,8 @@ import {ComponentTypes}  from "../src/Enum/ComponentTypes";
 describe("RobotComponent", () => {
 	it("should build a head component to our specification", () => {
 		let testedRC = new RobotComponent(ComponentTypes.Head, 5, 7);
-		expect(testedRC.getType).toEqual(ComponentTypes.Head);
-		expect(testedRC.getSpeed()).toEqual(5);
-		expect(testedRC.getStrength()).toEqual(7);
+		expect(testedRC.getType()).toEqual(ComponentTypes.Head);
+		expect(testedRC.getStrength()).toEqual(5);
+		expect(testedRC.getSpeed()).toEqual(7);
 	});
 });
